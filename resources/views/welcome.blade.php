16f1c54c465d839b69bcf900ff93e1d7895d2db3
laodesumardi <sumardilaode04@gmail.com>
2025-11-16 14:00:47 +0900

    Landing: tambah menu 'CBT Sekolah' menuju domain CBT

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,6 +42,12 @@
                                         href="https://cbt.smpnegeri01namrole.sch.id/login"
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
                                     >
+                                        CBT Sekolah
+                                    </a>
+                                    <a
+                                        href="https://cbt.smpnegeri01namrole.sch.id/login"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >
                                         Log in
                                     </a>
 
