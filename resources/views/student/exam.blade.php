02d306543faa5f36476855ad0cb6e108ef6b7201
laodesumardi <sumardilaode04@gmail.com>
2026-04-16 03:03:16 +0900

    tampilan

--- resources/views/student/exam.blade.php
+++ resources/views/student/exam.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, viewport-fit=cover">
     <title>Ujian Online - Mode Aman</title>
+     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         * {
             margin: 0;
